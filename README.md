4cfb3a3d9315f420acc76afd4a0db95d3ebad98d
Dan Tran <67458114+autumnssuns@users.noreply.github.com>
2023-07-12 12:27:15 +1000

    Added project code

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
+# Guess The Number
 
 > Open this page at [https://qut008.github.io/w2d2_guess_the_number/](https://qut008.github.io/w2d2_guess_the_number/)
-
-## Use as Extension
-
-This repository can be added as an **extension** in MakeCode.
-
-* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
-* click on **New Project**
-* click on **Extensions** under the gearwheel menu
-* search for **https://github.com/qut008/w2d2_guess_the_number** and import
 
 ## Edit this project
 
@@ -17,6 +9,23 @@ To edit this repository in MakeCode.
 * open [https://makecode.microbit.org/](https://makecode.microbit.org/)
 * click on **Import** then click on **Import URL**
 * paste **https://github.com/qut008/w2d2_guess_the_number** and click import
+
+## The Idea
+
+Guess the number is a simple game where the host thinks of a number and the player tries to guess it. If the player guesses correctly, they win. If they guess incorrectly, the host will tell them if they need to go higher or lower.
+
+This project is a micro:bit version of the game, where one micro:bit is the host and the other is the player.
+
+## Running the game
+
+1. On makecode.microbit.org, import the project from the URL: 
+2. Press the `Logo` button to start the host micro:bit.
+3. The host micro:bit will display a random number between 0 and 9, if you want to adjust the number, press the `A` button to go lower or the `B` button to go higher.
+4. Confirm the number by pressing the `Logo` button again, this will initialise the player micro:bit.
+5. On the player micro:bit, press the `A` button to go lower or the `B` button to go higher, and press the `Logo` button to confirm your guess.
+6. The host micro:bit will display if the player needs to go higher or lower.
+7. After receiving the feedback, the player micro:bit will provide you a suggestion for your next guess.
+8. Repeat steps 5-7 until the player guesses the correct number, then the host micro:bit will display a tick.
 
 #### Metadata (used for search, rendering)
 
--- main.ts
+++ main.ts
@@ -1,1 +1,87 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showIcon(IconNames.Heart)
+    if (key < receivedNumber) {
+        basic.showArrow(ArrowNames.South)
+        radio.sendString("down")
+    } else if (key > receivedNumber) {
+        basic.showArrow(ArrowNames.North)
+        radio.sendString("up")
+    } else {
+        basic.showIcon(IconNames.Yes)
+        radio.sendString("correct")
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    if (isHost == 1) {
+        key = (key - 1) % (max + 1)
+        basic.showNumber(key)
+    }
+    if (isPlayer == 1) {
+        guess = (guess - 1) % (max + 1)
+        basic.showNumber(guess)
+    }
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "start") {
+        basic.showIcon(IconNames.Heart)
+        strip = neopixel.create(DigitalPin.P0, max + 1, NeoPixelMode.RGB_RGB)
+        strip.showRainbow(1, 120)
+        low = 0
+        high = max
+        isPlayer = 1
+    }
+    if (receivedString == "correct") {
+        range = strip.range(guess + 1, max + 1)
+        range.clear()
+        range = strip.range(0, guess)
+        range.clear()
+        basic.showNumber(guess)
+    } else {
+        if (receivedString == "up") {
+            range = strip.range(0, guess + 1)
+            range.clear()
+            low = guess + 1
+        }
+        if (receivedString == "down") {
+            range = strip.range(guess, max + 1)
+            range.clear()
+            high = guess - 1
+        }
+        guess = Math.floor((low + high) / 2)
+        basic.showNumber(guess)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (isHost == 1) {
+        key = (key + 1) % (max + 1)
+        basic.showNumber(key)
+    }
+    if (isPlayer == 1) {
+        guess = (guess + 1) % (max + 1)
+        basic.showNumber(guess)
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (isHost + isPlayer == 0) {
+        isHost = 1
+        key = randint(0, max)
+        basic.showNumber(key)
+    }
+    if (isHost == 1) {
+        radio.sendString("start")
+    }
+    if (isPlayer == 1) {
+        radio.sendNumber(guess)
+    }
+})
+let range: neopixel.Strip = null
+let high = 0
+let low = 0
+let strip: neopixel.Strip = null
+let guess = 0
+let isPlayer = 0
+let isHost = 0
+let key = 0
+let max = 0
+radio.setGroup(1)
+max = 9
