9ac78bbfb1b6bfccdb554f8a8cd1a05c87d12049
Dan Tran <67458114+autumnssuns@users.noreply.github.com>
2023-07-12 15:27:34 +1000

    Added some functions

--- main.ts
+++ main.ts
@@ -21,34 +21,30 @@ input.onButtonPressed(Button.A, function () {
         basic.showNumber(guess)
     }
 })
+function celebrate () {
+    range = strip.range(guess + 1, max + 1)
+    range.clear()
+    range = strip.range(0, guess)
+    range.clear()
+    basic.showNumber(guess)
+}
+function initialiseHost () {
+    isHost = 1
+    key = randint(0, max)
+    basic.showNumber(key)
+}
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "start") {
-        basic.showIcon(IconNames.Heart)
-        strip = neopixel.create(DigitalPin.P0, max + 1, NeoPixelMode.RGB_RGB)
-        strip.showRainbow(1, 120)
-        low = 0
-        high = max
-        isPlayer = 1
+        initialisePlayer()
     }
     if (receivedString == "correct") {
-        range = strip.range(guess + 1, max + 1)
-        range.clear()
-        range = strip.range(0, guess)
-        range.clear()
-        basic.showNumber(guess)
-    } else {
-        if (receivedString == "up") {
-            range = strip.range(0, guess + 1)
-            range.clear()
-            low = guess + 1
-        }
-        if (receivedString == "down") {
-            range = strip.range(guess, max + 1)
-            range.clear()
-            high = guess - 1
-        }
-        guess = Math.floor((low + high) / 2)
-        basic.showNumber(guess)
+        celebrate()
+    }
+    if (receivedString == "up") {
+        makeHigherGuess()
+    }
+    if (receivedString == "down") {
+        makeLowerGuess()
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -61,11 +57,24 @@ input.onButtonPressed(Button.B, function () {
         basic.showNumber(guess)
     }
 })
+function makeHigherGuess () {
+    range = strip.range(0, guess + 1)
+    range.clear()
+    low = guess + 1
+    guess = Math.floor((low + high) / 2)
+    basic.showNumber(guess)
+}
+function initialisePlayer () {
+    basic.showIcon(IconNames.Heart)
+    strip = neopixel.create(DigitalPin.P0, max + 1, NeoPixelMode.RGB_RGB)
+    strip.showRainbow(1, 120)
+    low = 0
+    high = max
+    isPlayer = 1
+}
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     if (isHost + isPlayer == 0) {
-        isHost = 1
-        key = randint(0, max)
-        basic.showNumber(key)
+        initialiseHost()
     }
     if (isHost == 1) {
         radio.sendString("start")
@@ -74,10 +83,17 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
         radio.sendNumber(guess)
     }
 })
-let range: neopixel.Strip = null
+function makeLowerGuess () {
+    range = strip.range(guess, max + 1)
+    range.clear()
+    high = guess - 1
+    guess = Math.floor((low + high) / 2)
+    basic.showNumber(guess)
+}
 let high = 0
 let low = 0
 let strip: neopixel.Strip = null
+let range: neopixel.Strip = null
 let guess = 0
 let isPlayer = 0
 let isHost = 0
